c38bc9679e8dce02b636d67c9c018b3479c1ff17
Jonathan Cheung Budd <CheuJona853@GoTVDSB.ca>
2022-03-10 10:33:59 -0500

    Stuff

--- Part 5 - Console Decisions/Program.cs
+++ Part 5 - Console Decisions/Program.cs
@@ -6,17 +6,63 @@ namespace Part_5___Console_Decisions
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /////////////////////////////////////////////////////////////////////////// Stages
+            {
+                Console.WriteLine("----Stages----");
+
+                int age;
+                Console.WriteLine("Enter your age: ");
+                age = Convert.ToInt32(Console.ReadLine());
+
+
+
+                if (age < 0)
+                {
+                    Console.WriteLine("Error");
+                }
+                else if (age <= 5)
+                {
+                    Console.WriteLine("Child");
+                }
+                else if (age < 19)
+                {
+                    Console.WriteLine("Toddler");
+                }
+                else if (age > 18)
+                {
+                    Console.WriteLine("Adult");
+                }
+                else if (age <= 10)
+                {
+                    Console.WriteLine("Preteen");
+                }
+                else if (age <= 12)
+                {
+                    Console.WriteLine("Teen");
+
+
+
+                }
+            }
+            /////////////////////////////////////////////////////////////////////////// Hurricane
+            {
+                Console.WriteLine("----Hurricane----");
 
 
 
 
 
+            }
+            /////////////////////////////////////////////////////////////////////////// Dice Game
+            {
+                Console.WriteLine("----Dice Game----");
+
+            
+            
+            }
 
 
-
-
-
+            Console.ReadKey();
         }
     }
 }
