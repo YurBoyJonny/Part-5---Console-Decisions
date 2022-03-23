ab472e7b875df5e44dc5f8d5b8a97abb47f9d367
cheujona853 <cheujona853@2260I-L42-74>
2022-03-23 11:04:17 -0400

    more newnew

--- Part 5 - Console Decisions/Program.cs
+++ Part 5 - Console Decisions/Program.cs
@@ -1,72 +1,93 @@
 ﻿using System;
+////////////////////////////////////////////////////////////////// MENU //////////////////////////////////////////////////////////////////
 namespace Part_5___Console_Decisions
 {
     class Program
     {
         static void Main(string[] args)
-        {
-            ////////////////////////////////////////////////////////////////// MENU //////////////////////////////////////////////////////////////////
+        {             
+            Console.WriteLine("Type 1 for STAGES, 2 for HURRICANE, or 3 for DICE GAME.");
+            int menu = Convert.ToInt32(Console.ReadLine());
+            if (menu == 1)
             {
-                {
-                    Console.WriteLine("Type 1 for STAGES, 2 for HURRICANE, or 3 for DICE GAME.");
-                    int menu = Convert.ToInt32(Console.ReadLine());
-                    if (menu == 1)
-                    {
-                        Console.WriteLine("Stages");
-                    }
-                    else if (menu > 1)
-                    {
-                        Console.WriteLine("Error");
-                    }
-                    else if (menu < 1)
-                    {
-                        Console.WriteLine("Error");
-                    }
-
-                }
+                Stages();
             }
+            if (menu == 2)
+            {
+
+            }
+            if (menu == 3)
+            {
+                Dice();
+            }
+            else if (menu > 3)
+            {
+                Console.WriteLine("Error");
+            }
+            else if (menu < 1)
+            {
+                Console.WriteLine("Error");
+            }
+
+            
+            
+
             ////////////////////////////////////////////////////////////////// STAGES //////////////////////////////////////////////////////////////////
-            {
-                Console.WriteLine("----Stages----");
-                int age;
-                Console.WriteLine("Enter your age: ");
-                age = Convert.ToInt32(Console.ReadLine());
-                if (age > 18)
-                {
-                    Console.WriteLine("Adult");
-                }
-                else if (age < 0)
-                {
-                    Console.WriteLine("Error");
-                }
-                else if (age <= 5)
-                {
-                    Console.WriteLine("Toddler");
-                }
-                else if (age <= 10)
-                {
-                    Console.WriteLine("Child");
-                }
-                else if (age <= 12)
-                {
-                    Console.WriteLine("Preteen");
-                }
-                else if (age > 12)
-                {
-                    Console.WriteLine("Teen");
-                }
-            }
-            ////////////////////////////////////////////////////////////////// HURRICANE //////////////////////////////////////////////////////////////////
-            {
-                Console.WriteLine("----Hurricane----");
-                Console.Write("Enter a hurricane category level out of; 1, 2, 3, 4, 5 ");
-                int userCategory = Convert.ToInt32(Console.ReadLine());
-
+            
                 
+           
+            
+        }
 
+        public static void Stages()
+        {
+            Console.WriteLine("----Stages----");
+            int age;
+            Console.WriteLine("Enter your age: ");
+            age = Convert.ToInt32(Console.ReadLine());
+            if (age > 18)
+            {
+                Console.WriteLine("Adult");
             }
+            else if (age < 0)
+            {
+                Console.WriteLine("Error");
+            }
+            else if (age <= 5)
+            {
+                Console.WriteLine("Toddler");
+            }
+            else if (age <= 10)
+            {
+                Console.WriteLine("Child");
+            }
+            else if (age <= 12)
+            {
+                Console.WriteLine("Preteen");
+            }
+            else if (age > 12)
+            {
+                Console.WriteLine("Teen");
+            }
+        }
+
+        public static void Hurricane() 
+        { 
+            ////////////////////////////////////////////////////////////////// HURRICANE //////////////////////////////////////////////////////////////////
+
+            Console.WriteLine("----Hurricane----");
+            Console.Write("Enter a hurricane category level out of; 1, 2, 3, 4, 5 ");
+            int userCategory = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public static void Dice()
+        {
+            
+
+
             ////////////////////////////////////////////////////////////////// DICE GAME //////////////////////////////////////////////////////////////////
             {
+
                 string diceOne = (@"-----
 |   |
 | o |
@@ -103,45 +124,72 @@ namespace Part_5___Console_Decisions
 -----
 ");
                 Console.WriteLine("----Dice Game----");
+
                 Double money = 100;
-                Console.WriteLine("You have $" + money + " in your bank account. Let's feed your gambling addiction :)");
-                Console.WriteLine("How much do you bet?");
+                Console.WriteLine("You have $" + money + " in your bank account. How Much do you bet?");
                 Double bet = Convert.ToDouble(Console.ReadLine());
-                Console.WriteLine("What side of the dice do you call?");
-                int diceSide = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Are you sure you want to choose " + diceSide + " with a bet of $" + bet + "?");
-                string answer = Console.ReadLine();
-                if (answer is "no")
-                    Console.WriteLine("Okay then ):");
-                else
-                    Console.WriteLine("Okay then :)");
 
 
-
-                    
-                Console.WriteLine(diceOne);
-                Console.WriteLine(diceTwo);
-                Console.WriteLine(diceThree);
-                Console.WriteLine(diceFour);
-                Console.WriteLine(diceFive);
-                Console.WriteLine(diceSix);
 
                 double winnings = 0;
-
                 double doubles = (bet * 2);
                 double notDouble = (bet * 1.5);
                 double evenSum = bet;
                 double oddSum = bet;
 
+                Console.WriteLine("What side of the dice do you call?"); ///////////////////////////// 
+                int diceSide = Convert.ToInt32(Console.ReadLine());
 
-                double moneyLeft = bet +- winnings;
+                Random rnd = new Random();
+                int diceRolled = rnd.Next(1, 7);
+                Console.WriteLine("The die landed on " + diceRolled); ///////////////////////////////// TWO DICE NEEDED
+                if (diceRolled == 1)
+                {
+                    Console.WriteLine(diceOne);
+                }
+                else if (diceRolled == 2)
+                {
+                    Console.WriteLine(diceTwo);
+                }
+                else if (diceRolled == 3)
+                {
+                    Console.WriteLine(diceThree);
+                }
+                else if (diceRolled == 4)
+                {
+                    Console.WriteLine(diceFour);
+                }
+                else if (diceRolled == 5)
+                {
+                    Console.WriteLine(diceFive);
+                }
+                else if (diceRolled == 6)
+                {
+                    Console.WriteLine(diceSix);
+                }
+
+
+                if (diceSide == diceRolled)
+                {
+                    Console.WriteLine("Win!");
+
+                }
+                if (diceSide != diceRolled)
+                {
+                    Console.WriteLine("Lose!");
+                }
+
+
+
+                double moneyLeft = bet + -winnings;
 
                 Console.WriteLine("You have: $" + moneyLeft + " left");
 
-                
 
+
+
+                Console.ReadKey();
             }
-            Console.ReadKey();
         }
     }
 }
