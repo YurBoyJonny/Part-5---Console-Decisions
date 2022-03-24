148ff7c4e20c85a4844fc60ecd6f08bad5fc2542
cheujona853 <cheujona853@2260I-L42-74>
2022-03-24 10:56:39 -0400

    almost done

--- Part 5 - Console Decisions/Program.cs
+++ Part 5 - Console Decisions/Program.cs
@@ -5,7 +5,7 @@ namespace Part_5___Console_Decisions
     class Program
     {
         static void Main(string[] args)
-        {             
+        {
             Console.WriteLine("Type 1 for STAGES, 2 for HURRICANE, or 3 for DICE GAME.");
             int menu = Convert.ToInt32(Console.ReadLine());
             if (menu == 1)
@@ -14,7 +14,7 @@ namespace Part_5___Console_Decisions
             }
             if (menu == 2)
             {
-
+                Hurricane();
             }
             if (menu == 3)
             {
@@ -28,17 +28,8 @@ namespace Part_5___Console_Decisions
             {
                 Console.WriteLine("Error");
             }
-
-            
-            
-
-            ////////////////////////////////////////////////////////////////// STAGES //////////////////////////////////////////////////////////////////
-            
-                
-           
-            
         }
-
+////////////////////////////////////////////////////////////////// STAGES //////////////////////////////////////////////////////////////////
         public static void Stages()
         {
             Console.WriteLine("----Stages----");
@@ -70,126 +61,153 @@ namespace Part_5___Console_Decisions
                 Console.WriteLine("Teen");
             }
         }
-
-        public static void Hurricane() 
-        { 
-            ////////////////////////////////////////////////////////////////// HURRICANE //////////////////////////////////////////////////////////////////
-
+        ////////////////////////////////////////////////////////////////// HURRICANE //////////////////////////////////////////////////////////////////
+        public static void Hurricane()
+        {
             Console.WriteLine("----Hurricane----");
-            Console.Write("Enter a hurricane category level out of; 1, 2, 3, 4, 5 ");
+            Console.WriteLine("Enter a hurricane category level out of; 1, 2, 3, 4, 5 ");
             int userCategory = Convert.ToInt32(Console.ReadLine());
-        }
 
+            Console.Write("This hurricane category is ");
+            switch (userCategory)
+            {
+                case 1:
+                    Console.WriteLine("74-95 mph or 64-82 kt or 119-153 km/hr");
+                    break;
+                case 2:
+                    Console.WriteLine("96-110 mph or 83-95 kt or 154-177 km/hr");
+                    break;
+                case 3:
+                    Console.WriteLine("111-130 mph or 96-113 kt or 178-209 km/hr");
+                    break;
+                case 4:
+                    Console.WriteLine("131-155 mph or 114-135 kt or 210-249 km/hr");
+                    break;
+                case 5:
+                    Console.WriteLine("Greater than 155 mph or 135 kt or 249 km/hr");
+                    break;
+                default:
+                    break;
+            }
+        }
+////////////////////////////////////////////////////////////////// DICE GAME //////////////////////////////////////////////////////////////////
         public static void Dice()
         {
-            
-
-
-            ////////////////////////////////////////////////////////////////// DICE GAME //////////////////////////////////////////////////////////////////
-            {
-
-                string diceOne = (@"-----
+            string diceOne = (@"-----
 |   |
 | o |
 |   |
 -----");
-                string diceTwo = (@"-----
+            string diceTwo = (@"-----
 |o  |
 |   |
 |  o|
 -----
 ");
-                string diceThree = (@"-----
+            string diceThree = (@"-----
 |o  |
 | o |
 |  o|
 -----
 ");
-                string diceFour = (@"-----
+            string diceFour = (@"-----
 |o o|
 |   |
 |o o|
 -----
 ");
-                string diceFive = (@"-----
+            string diceFive = (@"-----
 |o o|
 | o |
 |o o|
 -----
 ");
-                string diceSix = (@"-----
+            string diceSix = (@"-----
 |o o|
 |o o|
 |o o|
 -----
 ");
-                Console.WriteLine("----Dice Game----");
+            Console.WriteLine("----Dice Game----");
 
-                Double money = 100;
-                Console.WriteLine("You have $" + money + " in your bank account. How Much do you bet?");
-                Double bet = Convert.ToDouble(Console.ReadLine());
+            Double money = 100;
+            Console.WriteLine("You have $" + money + " in your bank account. How Much do you bet?");
+            Double bet = Convert.ToDouble(Console.ReadLine());
 
-
-
-                double winnings = 0;
-                double doubles = (bet * 2);
-                double notDouble = (bet * 1.5);
-                double evenSum = bet;
-                double oddSum = bet;
-
-                Console.WriteLine("What side of the dice do you call?"); ///////////////////////////// 
-                int diceSide = Convert.ToInt32(Console.ReadLine());
-
-                Random rnd = new Random();
-                int diceRolled = rnd.Next(1, 7);
-                Console.WriteLine("The die landed on " + diceRolled); ///////////////////////////////// TWO DICE NEEDED
-                if (diceRolled == 1)
-                {
-                    Console.WriteLine(diceOne);
-                }
-                else if (diceRolled == 2)
-                {
-                    Console.WriteLine(diceTwo);
-                }
-                else if (diceRolled == 3)
-                {
-                    Console.WriteLine(diceThree);
-                }
-                else if (diceRolled == 4)
-                {
-                    Console.WriteLine(diceFour);
-                }
-                else if (diceRolled == 5)
-                {
-                    Console.WriteLine(diceFive);
-                }
-                else if (diceRolled == 6)
-                {
-                    Console.WriteLine(diceSix);
-                }
-
-
-                if (diceSide == diceRolled)
-                {
-                    Console.WriteLine("Win!");
-
-                }
-                if (diceSide != diceRolled)
-                {
-                    Console.WriteLine("Lose!");
-                }
-
-
-
-                double moneyLeft = bet + -winnings;
-
-                Console.WriteLine("You have: $" + moneyLeft + " left");
-
-
-
-
-                Console.ReadKey();
+            if (bet < 0 || bet > 100)
+            {
+                bet = 0;
             }
+
+            double winnings = 0;
+            double doubles = (bet * 2);
+            double notDouble = (bet * 1.5);
+            double evenSum = (bet * 2);
+            double oddSum = (bet * 2);
+
+
+            Console.WriteLine("Type 1 for doubles, or 2 for not double.");
+            int outcomeBet1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Type 3 for even sum, or 4 for odd sum.");
+            int outcomeBet2 = Convert.ToInt32(Console.ReadLine());
+
+            Random rnd = new Random();
+            int diceRolled1 = rnd.Next(1, 7);
+            new Random();
+            int diceRolled2 = rnd.Next(1, 7);
+            Console.WriteLine("You got " + diceRolled1 + " and " + diceRolled2);
+
+            if (diceRolled1 == 1 || diceRolled2 == 1)
+            {
+                Console.WriteLine(diceOne);
+            }
+            if (diceRolled1 == 2 || diceRolled2 == 2)
+            {
+                Console.WriteLine(diceTwo);
+            }
+            if (diceRolled1 == 3 || diceRolled2 == 3)
+            {
+                Console.WriteLine(diceThree);
+            }
+            if (diceRolled1 == 4 || diceRolled2 == 4)
+            {
+                Console.WriteLine(diceFour);
+            }
+            if (diceRolled1 == 5 || diceRolled2 == 5)
+            {
+                Console.WriteLine(diceFive);
+            }
+            if (diceRolled1 == 6 || diceRolled2 == 6)
+            {
+                Console.WriteLine(diceSix);
+            }
+
+
+
+            if (outcomeBet1 == 1 && diceRolled1 == diceRolled2)
+            {
+                bet = Convert.ToDouble(doubles);
+            }
+            else if (outcomeBet1 == 2 && diceRolled1 != diceRolled2)
+            {
+                bet = Convert.ToDouble(notDouble);
+            }
+            if (outcomeBet2 == 3)
+            {
+                bet = Convert.ToDouble(evenSum);
+            }
+            else if (outcomeBet2 == 4)
+            {
+                bet = Convert.ToDouble(oddSum);
+            }
+
+            double moneyLeft = bet + winnings;
+
+            Console.WriteLine("You have: $" + moneyLeft + " left");
+
+            Console.ReadKey();
         }
     }
 }
+
